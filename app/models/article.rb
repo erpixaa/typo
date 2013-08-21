e54b97a61e16dc32ab9d9cb12c1218a65d81a232
Daniel A <daniel_arb69@hotmail.com>
2013-08-21 15:04:54 -0700

    merge comments, done

--- app/models/article.rb
+++ app/models/article.rb
@@ -132,11 +132,11 @@ class Article < Content
     end
 
     self.body += other_article.body
-    Article.find_by_id(other_article_id).delete
     other_article_id.comments.each do |comment|
       comment.article_id = self.id
       self.comments << comment
     end
+    Article.find_by_id(other_article_id).delete
     self.save
     true
   end

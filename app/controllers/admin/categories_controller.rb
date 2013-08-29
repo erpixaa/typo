8b6d092ddd8207ed901cd5a3eebb5125316877cf
Daniel A <daniel_arb69@hotmail.com>
2013-08-29 14:19:39 -0700

    Editting suport

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -27,7 +27,7 @@ class Admin::CategoriesController < Admin::BaseController
     @categories = Category.find(:all)
     #@category = Category.find(params[:id])
     @category = Category.find(params[:id]) if params[:id]
-    @category = Category.new
+    @category = Category.new unless params[:id]
     @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
